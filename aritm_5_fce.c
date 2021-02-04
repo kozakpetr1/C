3eec765fc3984799aa86efb090ac932c55d285e4
kozakpetr1 <kozak.petr@gmail.com>
2021-02-04 13:58:07 +0100

    Pouziti funkci

---
+++ aritm_5_fce.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double nacti() {
+    double hodnota;
+
+    printf("Zadej hodnotu: ");
+    scanf("%lf", &hodnota);
+    return hodnota;
+}
+
+void porovnani(double param_x, double param_y) {
+    printf("Vetsi cislo je %lf.\n", (param_x > param_y) ? param_x : param_y);
+}
+
+int main() {
+    char opakovani = 'a';
+    double prom_a, prom_b;
+
+    while (opakovani != 'n') {
+        printf("Porovnani  dvou cisel\n-----------------------------\n");
+        prom_a = nacti();
+        prom_b = nacti();
+        porovnani(prom_a, prom_b);
+        porovnani(15.5, 27.1);
+
+        printf("\nChces znovu porovnavat? [a/n] ");
+
+        do {
+            opakovani = (char) getchar();
+        } while (opakovani == '\n');
+
+    }
+    printf("\nPapa!");
+
+    return (EXIT_SUCCESS);
+}
