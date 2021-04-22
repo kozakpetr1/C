eac4c1d4307d6ce11f43a82cfc983f5221e23604
kozakpetr1 <kozak.petr@gmail.com>
2021-04-22 13:38:57 +0200

    Ukazatele - uvod

--- pointers.c
+++ pointers.c
@@ -19,12 +19,12 @@ int main() {
 
     p_b = &var_a; // do pointeru p_b vlož adresu proměnné var_a
 
-    printf("\e[1;32mvar_a = 5, var_b = 0;\e[0m\n");
+    printf("\e[1;32mint var_a = 5, var_b = 0;\e[0m\n");
     printf("\e[1;32mint *p_b;\e[0m\n\n");
 
     printf("\e[1;33mp_b = &var_a;\e[0m\n");
     printf("Adresa ulozena v pointeru \e[1;32mp_b\e[0m:                   %#010x\n", p_b);
-    printf("Hodnota lezici na adrese ulozene v pointeru \e[1;32mp_a\e[0m: %d\n", *p_b);
+    printf("Hodnota lezici na adrese ulozene v pointeru \e[1;32mp_b\e[0m: %d\n", *p_b);
     printf("Promenna \e[1;32mvar_a\e[0m lezi na adrese:                   %#010x\n", &var_a);
     printf("Hodnota ulozena v promenne \e[1;32mvar_a\e[0m:                %d\n", var_a);
 
@@ -33,11 +33,11 @@ int main() {
     printf("\n\e[1;33mvar_b = (int) p_b;\n\e[0m");
     printf("Hodnota ulozena v promenne \e[1;32mvar_b\e[0m:                %d\n\n", var_b);
 
-    p_b = &var_b; // do pointeru p_b vlož adresu proměnné var_a
+    p_b = &var_b; // do pointeru p_b vlož adresu proměnné var_b
 
     printf("\e[1;33mp_b = &var_b;\e[0m\n");
     printf("Adresa ulozena v pointeru \e[1;32mp_b\e[0m:                   %#010x\n", p_b);
-    printf("Hodnota lezici na adrese ulozene v pointeru \e[1;32mp_a\e[0m: %d\n", *p_b);
+    printf("Hodnota lezici na adrese ulozene v pointeru \e[1;32mp_b\e[0m: %d\n", *p_b);
     printf("Promenna \e[1;32mvar_b\e[0m lezi na adrese:                   %#010x\n", &var_b);
     printf("Hodnota ulozena v promenne \e[1;32mvar_b\e[0m:                %d\n", var_b);
 
